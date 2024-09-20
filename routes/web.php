281cde7bbafddb9aca97df0c5a5576d8a7047a31
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-19 20:42:20 -0400

    Add route for products catalog

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '<h1>Hello World</h1>';
 });
+
+Route::get('products', function () {
+    return 'Products Catalog';
+});
