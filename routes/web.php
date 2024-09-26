6f3335db6b75234c7017e9a552b6cd9810b2e471
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-26 11:21:47 -0400

    Refactor routes to include UserController for index method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('products', [ProductController::class, 'index']);
+Route::get('user', [UserController::class, 'index']);
 
 Route::get('products/{id}', [ProductController::class, 'show']);
 
