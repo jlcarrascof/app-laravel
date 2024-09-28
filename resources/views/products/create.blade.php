cac444313f92a9f9db2fb579585e829642d11dd7
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-09-28 18:50:35 -0400

    Refactor create.blade.php to include form for creating a new product

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layout/template')
+
+@section('title', 'New Product')
+
+@section('content')
+
+<form action="" method="POST">
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name">
+    </div>
+    <div class="mb-3">
+        <label for="price" class="form-label">Price</label>
+        <input type="text" class="form-control" id="price" name="price">
+    </div>
+
+</form>
