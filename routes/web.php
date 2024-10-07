c75ceaec6d5da6825d7678e97f5238c9f2f30370
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 19:22:41 -0400

    Add route for deleting a product

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('products/{id}', [ProductController::class, 'show']);
 Route::post('products', [ProductController::class, 'store']);
 Route::get('products/{id}/edit', [ProductController::class, 'edit']);
 Route::put('products/{id}', [ProductController::class, 'update']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
 
 Route::get('users', [UserController::class, 'index']);
 
