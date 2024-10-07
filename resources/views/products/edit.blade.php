59adb07589a537ddc59512e1287b32b5ed8782e4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 19:28:54 -0400

    Refactor edit view for product to include form for updating product details

---
+++ resources/views/products/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('layout/template')
+
+@section('title', 'Edit Product')
+
+@section('content')
+
+<form action="{{ url('/products/.$id') }}" method="POST">
+    @method('PUT')
+    @csrf
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name">
+    </div>
+    <div class="mb-3">
+        <label for="price" class="form-label">Price</label>
+        <input type="text" class="form-control" id="price" name="price">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Save</button>
+</form>
+
+@endsection
