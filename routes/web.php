80e8368e9f7b709d4267b4b7a128b03ca1fe5acb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 19:21:16 -0400

    Refactor routes/web.php to add routes for editing and updating a product

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::get('products', [ProductController::class, 'index']);
 Route::get('products/create', [ProductController::class, 'create']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::post('products', [ProductController::class, 'store']);
+Route::get('products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('products/{id}', [ProductController::class, 'update']);
 
 Route::get('users', [UserController::class, 'index']);
 
