45457f609ce4bd51d96dce83e3cb2179fb5e1a6b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-07 18:48:08 -0400

    Add route for creating a new product

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', function () {
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/create', [ProductController::class, 'create']);
 Route::get('products/{id}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
 
 Route::get('users', [UserController::class, 'index']);
 
