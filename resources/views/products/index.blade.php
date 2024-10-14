33fa6bc79a62e4ecd405d3b00808af7f9e610b8c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-13 21:45:29 -0400

    Refactor product index view to update stock column name

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -29,7 +29,7 @@
                     <td>{{ $item->code }}</td>
                     <td>{{ $item->description }}</td>
                     <td>{{ $item->price }}</td>
-                    <td>{{ $item->stock }}</td>
+                    <td>{{ $item->exist }}</td>
                     <td></td>
                     <td></td>
                 </tr>
